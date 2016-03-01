1276d8492c4b0c2be9e871759bd9011d969c2cbe
Willem Seynaeve <willem.seynaeve@student.kuleuven.be>
2016-03-01 23:03:46 +0100

    Made helper functions transition to working attacking retsing stamina and inRangeForAttack

--- OGP1516-Hillbillies/src/hillbillies/model/Unit.java
+++ OGP1516-Hillbillies/src/hillbillies/model/Unit.java
@@ -1,5 +1,6 @@
 package hillbillies.model;
 import java.util.*;
+
 import java.lang.Math;
 
 import ogp.framework.util.*;
@@ -702,53 +703,6 @@ public class Unit {
 	}
 	
 
-	/* Helper methods */
-	
-	/**
-	 * 
-	 * @param victim
-	 * 			|The unit you want to attack
-	 * @return True if and only if the position of victim is a neighbouring cube of the position of this unit 
-	 * 			|for (i = 0; i < this.getPosition().length ; i++)
-	 * 			|	this.getPosition()[i] == victim.getPosition()[i] + 1 || this.getPositon()[i] == victim.getPosition()[i] - 1
-	 * 			|
-	 */
-	private boolean inRangeForAttack(Unit victim){
-		for ( int i = 0; i < this.getPosition().length; i++){
-			if (this.getPosition()[i] == victim.getPosition()[i] + 1 || this.getPosition()[i] == victim.getPosition()[i] - 1)
-				return true;
-		}return false;
-	}
-	
-	
-	/**
-	 * Set the state of this unit to Attacking
-	 */
-	private void transitionToAttacking(){
-		this.state = ATTACKING;
-		this.setFlagsLow();
-	}
-	
-	
-	
-	/**
-	 * Set the state of this unit to Attacking
-	 */
-	private void transitionToWorking(){
-		this.state = WORKING;
-		this.setFlagsLow();
-	}
-	
-	/**
-	 * set all the flags low
-	 */
-	private void setFlagsLow(){
-		this.shouldWork = false;
-		this.shouldRest = false;
-		this.shouldAttack = false;
-	}
-	
-
 
 	/**
 	 * Return the state of this unit.
@@ -793,11 +747,94 @@ public class Unit {
 		this.state = state;
 	}
 
+/* Helper methods */
+	
+	/**
+	 * 
+	 * @param victim
+	 * 			|The unit you want to attack
+	 * @return True if and only if the position of victim is a neighbouring cube of the position of this unit 
+	 * 			|for (i = 0; i < this.getPosition().length ; i++)
+	 * 			|	this.getPosition()[i] == victim.getPosition()[i] + 1 || this.getPositon()[i] == victim.getPosition()[i] - 1
+	 * 			|
+	 */
+	private boolean inRangeForAttack(Unit victim){
+		for ( int i = 0; i < this.getPosition().length; i++){
+			if (this.getPosition()[i] == victim.getPosition()[i] + 1 || this.getPosition()[i] == victim.getPosition()[i] - 1)
+				return true;
+		}return false;
+	}
+	
+	/**
+	 * Set the state of this unit to RESTING
+	 */
+	private void transitionToRestingSamina(){
+		this.state = state.RESTING_STAMINA;
+		this.setFlagsLow();
+		this.restingStaminaCountdown = getRestingStaminaTime();
+	}
+	
+	private float getRestingStaminaTime() {
+		return ;
+	}
+	/**
+	 * Set the state of this unit to Attacking
+	 */
+	private void transitionToAttacking(){
+		this.state = state.ATTACKING;
+		this.setFlagsLow();
+		this.attackingCountdown = getAttacktime();
+	}
+	
+	
+	/**
+	 * The time it takes for a unit to attack
+	 * @return
+	 */
+	private float getAttacktime() {
+		return 1;
+	}
+	
+	/**
+	 * Set the state of this unit to Attacking
+	 */
+	private void transitionToWorking(){
+		this.state = state.WORKING;
+		this.workingCountdown = getWorkTime();
+		this.setFlagsLow();
+	}
+	
+	/**
+	 * The worktime for this unit
+	 */
+	public float getWorkTime(){
+		return 500/this.getStrength();
+	}
+	
+	/**
+	 * set all the flags low
+	 */
+	private void setFlagsLow(){
+		this.shouldWork = false;
+		this.shouldRest = false;
+		this.shouldAttack = false;
+	}
+
+	/**
+	 * The time it will take before the next whole point of stamina is restored by resting
+	 */
+	private float restingStaminaCountdown;
+	/**
+	 * The time it will take before the work is done
+	 */
+	private float workingCountdown;
+	/**
+	 * The time it will take before the attack is actually carried out
+	 */
+	private float attackingCountdown;
 	/**
 	 * Variable registering the state of this unit.
 	 */
-
-
 	private boolean shouldRest;
 	private boolean shouldWork;
 	private boolean shouldAttack;
