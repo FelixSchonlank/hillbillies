bf6dc939e2f9e57730b4bda67ec65b8aa37bf77c
Willem Seynaeve <willem.seynaeve@student.kuleuven.be>
2016-03-05 21:06:59 +0100

    implemented the unit tests for Unit

---
+++ OGP1516-Hillbillies/src/hillbillies/model/TestUnit.java
@@ -0,0 +1,54 @@
+package hillbillies.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestUnit {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testAdvanceTime() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMoveToAdjacent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMoveTo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAttack() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDefend() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWork() {
+		fail("Not yet implemented");
+	}
+
+}
