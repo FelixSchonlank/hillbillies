66c447bb33c0d657aebd82880e10c8607adb5513
Felix Schönlank <felix.schonlank@gmail.com>
2016-03-28 13:45:08 +0200

    Moved methods from Unit to VectorUtils according to reorganization in actieplan.

--- OGP1516-Hillbillies/src/hillbillies/model/Unit.java
+++ OGP1516-Hillbillies/src/hillbillies/model/Unit.java
@@ -1689,23 +1689,6 @@ public class Unit {
 	}
 	
 	/**
-	 * Checks whether x lies in between y and z. It doesn't matter if y is to
-	 * its left and z to its right or the other way around; both situations
-	 * count.
-	 * @param x
-	 *        The value that should be in the middle
-	 * @param y
-	 *        One of the ends
-	 * @param z
-	 *        The other end
-	 * @return true iff x is between y and z.
-	 *        | result == (y < x && x < z) || (z < x && x < y)
-	 */
-	private static boolean between(double x, double y, double z){
-		return (y < x && x < z) || (z < x && x < y);
-	}
-	
-	/**
 	 * 
 	 * @param victim
 	 * 			|The unit you want to attack
@@ -2134,29 +2117,6 @@ public class Unit {
 	@Basic
 	private static double getMaxDT() {
 		return maxDT;
-	}
-	
-	/**
-	 * Gives the given vector, but normalized to have magnitude 1.
-	 * @param vector
-	 * 		The vector to be normalized
-	 * @return
-	 * 		The normalized version of the vector
-	 */
-	private static double[] normalize(double[] vector) {
-		int length = vector.length;
-		
-		double magnitude = 0;
-		for(int i=0; i<length; i++){
-			magnitude += Math.pow(vector[i], 2);
-		}
-		magnitude = Math.sqrt(magnitude);
-		
-		double[] result = new double[length];
-		for(int i=0; i<length; i++){
-			result[i] = vector[i] / magnitude;
-		}
-		return result;
 	}
 	
 	/**
