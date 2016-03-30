757a0b8edd60381c280998fd856de2f3d3deaa66
Felix Schönlank <felix.schonlank@gmail.com>
2016-03-31 00:48:32 +0200

    Fixed indentation with GameObject.java

--- OGP1516-Hillbillies/src/hillbillies/model/GameObject.java
+++ OGP1516-Hillbillies/src/hillbillies/model/GameObject.java
@@ -16,132 +16,132 @@ public class GameObject implements IAdvanceable{
 	 * 
 	 * @param  position
 	 *         The Position for this new GameObject.
- 	 * @effect The Position of this new GameObject is set to
- 	 *         the given Position.
- 	 *		| this.setPosition(position)
+	 * @effect The Position of this new GameObject is set to
+	 *         the given Position.
+	 *		| this.setPosition(position)
 	 */
 	public GameObject(Position position){
-		
+
 	}
 
 
-/**
- * Initialize this new GameObject with given Position.
- *
- * @param  position
- *         The Position for this new GameObject.
- * @effect The Position of this new GameObject is set to
- *         the given Position.
- *       | this.setPosition(position)
- */
-public GameObject(double[] position)
-		throws IllegalArgumentException {
-	this.setPosition(position);
-}
+	/**
+	 * Initialize this new GameObject with given Position.
+	 *
+	 * @param  position
+	 *         The Position for this new GameObject.
+	 * @effect The Position of this new GameObject is set to
+	 *         the given Position.
+	 *       | this.setPosition(position)
+	 */
+	public GameObject(double[] position)
+			throws IllegalArgumentException {
+		this.setPosition(position);
+	}
 
-/* Position */
+	/* Position */
 
-/**
- * Return the Position of this GameObject.
- */
-@Basic @Raw
-public Position getPosition() {
-	return this.position;
-}
+	/**
+	 * Return the Position of this GameObject.
+	 */
+	@Basic @Raw
+	public Position getPosition() {
+		return this.position;
+	}
 
-/**
- * Checks whether the given array representation of a position is a valid position for a Unit of this units world.
- *
- *@param position 
- *		The array representation of a position to check
- *@return True if and only if 
- *             the corresponding position is a valid position
- *             |for each i in position.length:
- *             |    if (! (position[i] >= getMinCoordinate() and position < getMaxCoordinate()))
- *             |		result == false
- *             | result == (result && position.length == 3)
- */
-@Raw
-public boolean canHaveAsPosition(double[] position){     
-    if (position.length != 3){
-    	return false;
-    }
-    Position positionToCheck = new Position(position);
-    return canHaveAsPosition(positionToCheck);                             
-}
+	/**
+	 * Checks whether the given array representation of a position is a valid position for a Unit of this units world.
+	 *
+	 *@param position 
+	 *		The array representation of a position to check
+	 *@return True if and only if 
+	 *             the corresponding position is a valid position
+	 *             |for each i in position.length:
+	 *             |    if (! (position[i] >= getMinCoordinate() and position < getMaxCoordinate()))
+	 *             |		result == false
+	 *             | result == (result && position.length == 3)
+	 */
+	@Raw
+	public boolean canHaveAsPosition(double[] position){     
+		if (position.length != 3){
+			return false;
+		}
+		Position positionToCheck = new Position(position);
+		return canHaveAsPosition(positionToCheck);                             
+	}
 
-/**
- * check whether the given position is a valid position for a unit of this units world
- * @param position
- * 			The position to check
- * @return true if and only if the position is within bounds of the units game world
- * 			| this.getWorld().withinBouds(position) ???
- */
-@Raw
-public boolean canHaveAsPosition(Position position){
-	return this.getWorld().withinBounds(position) && this.getWorld().isPassableCube(position.toCoordinate());
-}
+	/**
+	 * check whether the given position is a valid position for a unit of this units world
+	 * @param position
+	 * 			The position to check
+	 * @return true if and only if the position is within bounds of the units game world
+	 * 			| this.getWorld().withinBouds(position) ???
+	 */
+	@Raw
+	public boolean canHaveAsPosition(Position position){
+		return this.getWorld().withinBounds(position) && this.getWorld().isPassableCube(position.toCoordinate());
+	}
 
-/**
- * Set the Position of this GameObject to the given Position.
- * 
- * @param  position
- *         The new Position for this GameObject.
- * @post   The Position of this new GameObject is equal to
- *         the given Position.
- *       | new.getPosition() == position
- * @throws IllgalArgumentException
- *         The given Position is not a valid Position for any
- *         GameObject.
- *       | ! isValidPosition(getPosition())
- */
-@Raw
-public void setPosition(Position position) 
-		throws IllegalArgumentException {
-	if (! isValidPosition(position))
-		throw new IllegalArgumentException();
-	this.position = position;
-}
+	/**
+	 * Set the Position of this GameObject to the given Position.
+	 * 
+	 * @param  position
+	 *         The new Position for this GameObject.
+	 * @post   The Position of this new GameObject is equal to
+	 *         the given Position.
+	 *       | new.getPosition() == position
+	 * @throws IllgalArgumentException
+	 *         The given Position is not a valid Position for any
+	 *         GameObject.
+	 *       | ! isValidPosition(getPosition())
+	 */
+	@Raw
+	public void setPosition(Position position) 
+			throws IllegalArgumentException {
+		if (! isValidPosition(position))
+			throw new IllegalArgumentException();
+		this.position = position;
+	}
 
-/**                                                                                                                                                                                  
- * Set the position of this  unit to the given position.                                                                                                 
- *                                                                                                                                                                                   
- * @param  position
- *         The new position for this Unit.                                                                                                                       
- * @post   The position of this new Unit is equal to                                                                                                             
- *         the given position.                                                                                                                                              
- *       | new.getPosition() == position                                                                                                                           
- * @throws IllegalArgumentException                                                                                                                                                        
- *         The given position is not a valid position for any                                                                                                      
- *         Unit.                                                                                                                                                          
- *       | ! isValidPosition(position)                                                                                                                        
- */
-public void setPosition(double[] position)
-		throws IllegalArgumentException {
-	if (! isValidPosition(position))
-		throw new IllegalArgumentException(position + " is not a valid position.");
-	this.previousPosition = this.getPosition();
-	this.position = new Position(position);
-}
+	/**                                                                                                                                                                                  
+	 * Set the position of this  unit to the given position.                                                                                                 
+	 *                                                                                                                                                                                   
+	 * @param  position
+	 *         The new position for this Unit.                                                                                                                       
+	 * @post   The position of this new Unit is equal to                                                                                                             
+	 *         the given position.                                                                                                                                              
+	 *       | new.getPosition() == position                                                                                                                           
+	 * @throws IllegalArgumentException                                                                                                                                                        
+	 *         The given position is not a valid position for any                                                                                                      
+	 *         Unit.                                                                                                                                                          
+	 *       | ! isValidPosition(position)                                                                                                                        
+	 */
+	public void setPosition(double[] position)
+			throws IllegalArgumentException {
+		if (! isValidPosition(position))
+			throw new IllegalArgumentException(position + " is not a valid position.");
+		this.previousPosition = this.getPosition();
+		this.position = new Position(position);
+	}
 
-/**
- * Variable registering the Position of this GameObject.
- */
-private Position position;
-	
-/**
- * Holds the previous position of the Unit. Very important to determine whether it has reached its destination
- */
-protected Position previousPosition;
+	/**
+	 * Variable registering the Position of this GameObject.
+	 */
+	private Position position;
+
+	/**
+	 * Holds the previous position of the Unit. Very important to determine whether it has reached its destination
+	 */
+	protected Position previousPosition;
 
 
-public void advanceTime(double dt) throws IllegalArgumentException{
-	
-}
+	public void advanceTime(double dt) throws IllegalArgumentException{
 
-/**
- * 
- */
-protected Random random = new Random();
+	}
+
+	/**
+	 * 
+	 */
+	protected Random random = new Random();
 
 }
