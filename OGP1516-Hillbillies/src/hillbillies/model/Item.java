28973487ae5e08813d5b1525292c600a4f468939
Felix Schönlank <felix.schonlank@gmail.com>
2016-03-30 14:23:16 +0200

    Minor changes to Item.java

--- OGP1516-Hillbillies/src/hillbillies/model/Item.java
+++ OGP1516-Hillbillies/src/hillbillies/model/Item.java
@@ -5,9 +5,9 @@ import be.kuleuven.cs.som.annotate.Immutable;
 import be.kuleuven.cs.som.annotate.Raw;
 
 /**	 
-  * @invar  Each Item can have its weight as weight.
-  *       | canHaveAsWeight(this.getWeight())
-  */
+ * @invar  Each Item can have its weight as weight.
+ *       | canHaveAsWeight(this.getWeight())
+ */
 public class Item extends GameObject{
 	/**
 	 * Initialize a new Item with a given position and a random weight
@@ -17,123 +17,130 @@ public class Item extends GameObject{
 		int weight = random.nextInt(this.getMaxWeight() - this.getMinWeight()) + getMinWeight();
 		this.weight = weight;
 	}
-
-/* Weight */
-
-/**
- * Return the weight of this Item.
- */
-@Basic @Raw @Immutable
-public int getWeight() {
-	return this.weight;
-}
-
-/**
- * Check whether this Item can have the given weight as its weight.
- *  
- * @param  weight
- *         The weight to check.
- * @return 
- *       | result == true 
-*/
-@Raw
-public boolean canHaveAsWeight(int weight) {
-	return true;
-}
-
-/**
- * return the minimum weight of an Item 
- */
-public int getMinWeight(){
-	return this.MinWeight;
-}
-
-/**
- * Variable referencing the minimum weight for an Item
- */
-private int MinWeight = 50;
-
-/**
- * return the maximum weight of an Item 
- */
-public int getMaxWeight(){
-	return this.MaxWeight;
-}
-
-/**
- * Variable referencing the maximum weight for an Item
- */
-private int MaxWeight = 100;
-
-/**
- * Variable registering the weight of this Item.
- */
-private final int weight;
-
-/* Unit */
-
-/**
- * return the unit that is carrying this Item
- */
-public Unit getUnit(){
-	return this.unit;
-}
-
-/**
- * Set the unit that is carrying this Item to a given Unit
- */
-public void setUnit(Unit unit){
-	this.unit = unit;
-}
-
-/**
- * check whether the unit is being carried 
- */
-public boolean hasUnit(){
-	return this.getUnit() != null;
-}
-
-/**
- * a variable referencing the Unit that is carrying this Item
- */
-private Unit unit;
-
-
-
-/* Terminating */
-
-/**
- * Terminate this Item
- * 
- * @Post this Item is terminated
- * @Post The Item is no longer part f any World and is no longer carried by a Unit
- * @Post If the Item had a World or Unit the World no longer has this Item as Its Item or the Unit no longer has an Item 
- */
-public void terminate(){
-	if(! isTerminated()){
-		if (this.hasWorld()){
-			this.getWorld().removeItem(this);
-		}else if (this.hasUnit()){
-			//TODO the assotiation for unit
-		}
-		this.isTerminated = true;
-	}
-}
-
-/**
- * check whether this Item is terminated
- */
-public boolean isTerminated(){
-	return this.isTerminated;
-}
-
-/**
- * Variable registering whether this Item is terminated
- */
-private boolean isTerminated;
-
-public void advanceTime(double dt) throws IllegalArgumentException{
 	
-}
+	
+	
+	/* Weight */
+
+	/**
+	 * Return the weight of this Item.
+	 */
+	@Basic @Raw @Immutable
+	public int getWeight() {
+		return this.weight;
+	}
+
+	/**
+	 * Check whether this Item can have the given weight as its weight.
+	 *  
+	 * @param  weight
+	 *         The weight to check.
+	 * @return 
+	 *       | result == true 
+	 */
+	@Raw
+	public static boolean canHaveAsWeight(int weight) {
+		return (getMinWeight() <= weight 
+				&& weight <= getMaxWeight());
+	}
+
+	/**
+	 * return the minimum weight of an Item 
+	 */
+	@Immutable
+	public static int getMinWeight(){
+		return MinWeight;
+	}
+
+	/**
+	 * return the maximum weight of an Item 
+	 */
+	@Immutable
+	public static int getMaxWeight(){
+		return MaxWeight;
+	}
+
+	/**
+	 * Variable referencing the minimum weight for an Item
+	 */
+	private static final int MinWeight = 10;
+	
+	/**
+	 * Variable referencing the maximum weight for an Item
+	 */
+	private static final int MaxWeight = 50;
+
+	/**
+	 * Variable registering the weight of this Item.
+	 */
+	private final int weight;
+	
+	
+	
+	/* Unit */
+
+	/**
+	 * return the unit that is carrying this Item
+	 */
+	public Unit getUnit(){
+		return this.unit;
+	}
+
+	/**
+	 * Set the unit that is carrying this Item to a given Unit
+	 */
+	public void setUnit(Unit unit){
+		this.unit = unit;
+	}
+
+	/**
+	 * check whether the Item is being carried 
+	 */
+	public boolean hasUnit(){
+		return this.getUnit() != null;
+	}
+
+	/**
+	 * a variable referencing the Unit that is carrying this Item
+	 */
+	private Unit unit;
+
+
+
+	/* Terminating */
+
+	/**
+	 * Terminate this Item
+	 * 
+	 * @Post this Item is terminated
+	 * @Post The Item is no longer part of any World and is no longer carried by a Unit
+	 * @Post If the Item had a World or Unit the World no longer has this Item as its Item or the Unit no longer has an Item 
+	 */
+	public void terminate(){
+		if(! isTerminated()){
+			if (this.hasWorld()){
+				this.getWorld().removeItem(this);
+			}else if (this.hasUnit()){
+				//TODO the assotiation for unit
+			}
+			this.isTerminated = true;
+		}
+	}
+
+	/**
+	 * check whether this Item is terminated
+	 */
+	public boolean isTerminated(){
+		return this.isTerminated;
+	}
+
+	/**
+	 * Variable registering whether this Item is terminated
+	 */
+	private boolean isTerminated;
+
+	public void advanceTime(double dt) throws IllegalArgumentException{
+
+	}
 
 }
