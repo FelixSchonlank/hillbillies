721422c6c8c50786c69f990065a5877213fdac75
Felix Schönlank <felix.schonlank@gmail.com>
2016-05-11 22:18:39 +0200

    Added the WrongTypeException
    To be used when a ClassCastException was encountered in the
    Task program.

---
+++ OGP1516-Hillbillies/src/hillbillies/model/WrongTypeException.java
@@ -0,0 +1,21 @@
+package hillbillies.model;
+
+public class WrongTypeException extends Exception {
+	private static final long serialVersionUID = 1L;
+	
+	public WrongTypeException() {
+		super();
+	}
+	
+	public WrongTypeException(String message) {
+		super(message);
+	}
+	
+	public WrongTypeException(Throwable cause) {
+		super(cause);
+	}
+	
+	public WrongTypeException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
