93a343198be0387c1451bc29086c34c23f8bcfa6
Felix Schönlank <felix.schonlank@gmail.com>
2016-05-12 12:45:10 +0200

    Added Convert<T> extends Expression<T> class

---
+++ OGP1516-Hillbillies/src/hillbillies/model/expressions/Convert.java
@@ -0,0 +1,37 @@
+package hillbillies.model.expressions;
+
+import hillbillies.model.WrongTypeException;
+
+public class Convert<T> extends Expression<T> {
+
+	private final Expression<?> expression;
+	private final Class<T> clazz;
+	
+	/**
+	 * Constructs a new Convert object, which will convert its expression,
+	 * which can be parameterized to any type, to the desired type upon
+	 * evaluation. It will throw an error if the type cast appears to be
+	 * impossible.
+	 * @param expression
+	 * 		The expression that can be of any type
+	 * @param clazz
+	 * 		The class of the expression. For example: if you want a CarriesItem
+	 * 		expression to be converted, you should call
+	 * 		Convert(theCarriesItemExpression, Boolean.class)
+	 * 		btw, class was a reserved keyword so we had to use clazz.
+	 */
+	public Convert (Expression<?> expression, Class<T> clazz) {
+		this.expression = expression;
+		this.clazz = clazz;
+	}
+
+	@Override
+	public T evaluate () throws WrongTypeException {
+	    try {
+	        return this.clazz.cast(expression);
+	    } catch(ClassCastException e) {
+	    	throw new WrongTypeException(e);
+	    }
+	}
+
+}
